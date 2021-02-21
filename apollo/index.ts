e66c570e97fc296e82c690bc50cc260279081461
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2021-02-21 22:59:04 +0100

    Implement minimal express server

---
+++ apollo/index.ts
@@ -0,0 +1,10 @@
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+export const ConfigureApollo = (app: express.Application) => {
+    const apollo = new ApolloServer({ typeDefs, resolvers });
+
+    apollo.applyMiddleware({ app, path: '/apollo' });
+}
