d21a5b9b62dba0c6800c45a2ac4252bc2a6afe4b
Muhammad Farrukh Faizy <farrukh.faizy@gmail.com>
2021-03-03 19:30:06 +0100

    added password reset functionality

--- rest/index.ts
+++ rest/index.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import { PRIVATE_JWT_KEY } from '../utils/keys';
 import { AuthSchema } from '../utils/AuthValidation';
+import Joi from 'joi';
 
 const prisma = new PrismaClient();
 
@@ -83,8 +84,7 @@ export const ConfigureREST = (app: express.Application): void => {
                         const payload = { _id: user._id, email: user.email };
                         const token = jwt.sign(
                             { user: payload },
-                            PRIVATE_JWT_KEY,
-                            { expiresIn: '3 days' }
+                            PRIVATE_JWT_KEY
                         );
                         return res.json({ token });
                     });
@@ -92,6 +92,146 @@ export const ConfigureREST = (app: express.Application): void => {
                     return next(new Error(error.message));
                 }
             })(req, res, next);
+        }
+    );
+
+    restApi.post(
+        '/forgot',
+        passport.authenticate('jwt', { session: false }),
+        async (req: Request, res: Response, next: NextFunction) => {
+            /*
+            1 - Get email of the user
+            2 - Create a token
+            3 - Find the user with email in the DB
+            4 - Save the user id:token in the token table to later verify it
+            5 - Make the life of token not more than 5 minutes
+            6 - create a link associated with the generated token  
+            7 - Send the email to the email user has given 
+            */
+
+            const { email } = req.body;
+
+            const getUser = await prisma.user.findUnique({
+                where: {
+                    email: email,
+                },
+                select: {
+                    id: true,
+                    email: true,
+                    AuthenticationData: {
+                        select: {
+                            password: true,
+                        },
+                    },
+                },
+            });
+
+            if (!getUser) next(new Error('User not found'));
+
+            const payload = {
+                id: getUser?.id,
+                email: getUser?.email,
+            };
+
+            const secret =
+                getUser?.AuthenticationData[0].password +
+                '--' +
+                PRIVATE_JWT_KEY;
+
+            jwt.sign(payload, secret);
+        }
+    );
+
+    restApi.post(
+        '/reset/:userId/:token',
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const { userId, token } = req.params;
+                const { password } = req.body;
+
+                await prisma.user
+                    .findUnique({
+                        where: {
+                            id: userId,
+                        },
+                        include: {
+                            AuthenticationData: {
+                                select: {
+                                    id: true,
+                                    password: true,
+                                },
+                            },
+                        },
+                    })
+                    .then((user) => {
+                        if (user != null) {
+                            const secret =
+                                user.AuthenticationData[0].password +
+                                '--' +
+                                PRIVATE_JWT_KEY;
+                            const token__ = jwt.verify(token, secret);
+
+                            if (token__) {
+                                return {
+                                    user,
+                                    isPasswordMatch: bcrypt.compareSync(
+                                        password,
+                                        user.AuthenticationData[0].password
+                                    ),
+                                };
+                            } else if (token__ == '') {
+                                next(new Error('Invalid token/token expires'));
+                            } else {
+                                next(new Error('Invalid token'));
+                            }
+                        } else {
+                            next(new Error('User not found'));
+                        }
+                    })
+                    .then(async (obj) => {
+                        if (!obj?.isPasswordMatch) {
+                            await bcrypt.genSalt(10, (err, salt) => {
+                                return bcrypt.hash(
+                                    password,
+                                    salt,
+                                    async (err, hash) => {
+                                        if (err) throw err;
+
+                                        await prisma.authenticationData
+                                            .update({
+                                                where: {
+                                                    id:
+                                                        obj?.user
+                                                            .AuthenticationData[0]
+                                                            .id,
+                                                },
+                                                data: {
+                                                    password: hash,
+                                                },
+                                            })
+                                            .then((done) =>
+                                                res.json({
+                                                    msg:
+                                                        'User password has been updated',
+                                                })
+                                            );
+                                    }
+                                );
+                            });
+                        } else {
+                            next(
+                                new Error(
+                                    'New password cannot be an old password'
+                                )
+                            );
+                        }
+                    })
+                    .catch((err) => {
+                        next(new Error('Invalid token/token expires'));
+                    });
+            } catch (error) {
+                next(new Error(error.message));
+            }
         }
     );
 
