6e9c1fc714144422b8e5477daffb0b688b92bf45
Ayush Pandey <ayushpandey8439@gmail.com>
2021-04-16 00:56:16 +0200

    Unit tests for resetting passwords

--- tests/unit/rest/auth/userRegistration.test.ts
+++ tests/unit/rest/auth/userRegistration.test.ts
@@ -2,12 +2,13 @@ process.env.NODE_ENV = 'test';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 chai.use(chaiHttp);
-import { server } from './../../../server/index';
+import { server } from '../../../../server';
 import {
+    addUser,
     deleteUser,
     findUser,
     getUserCount,
-} from '../../../services/dataStore';
+} from '../../../../services/dataStore';
 
 describe('Checks if the auth route is available', function () {
     it('Checks if the auth route is setup properly.', function (done) {
@@ -30,8 +31,20 @@ describe('Checks if the auth route is available', function () {
 });
 
 describe('Try creating user', function () {
+    before(async function () {
+        const user = {
+            firstName: 'test',
+            lastName: 'user',
+            email: 'ayush.pandey@corona-school.de',
+            notificationLevel: 'all' as const,
+            phone: '+49017674853265',
+            password: 'password',
+        };
+        await addUser(user);
+    });
     after(async function () {
         await deleteUser('ayushpandey@corona-school.de');
+        await deleteUser('ayush.pandey@corona-school.de');
     });
     this.timeout(5000);
     it('Try creating a new user with an email that exists', (done) => {
